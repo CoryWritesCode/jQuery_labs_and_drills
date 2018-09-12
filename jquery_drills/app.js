1e0591d5b53980b4fa488aece8826415fd5704d4
CoryWritesCode <corycunningham2010@gmail.com>
2018-09-12 10:02:44 -0500

    fix it

--- jquery_drills/app.js
+++ jquery_drills/app.js
@@ -17,34 +17,45 @@ $(document).ready(function () {
     $('form').after('<div></div>');
     $('form').after('<ul>What is this?</ul>');
 
-    $(submitButton).click(function () { 
+    $(submitButton).on('click', function () { 
         // alert('You clicked me!'); <-- No longer used by the end.
         // $('<h2>Now look what you did.</h2>').appendTo('div'); <-- Same
-        $('ul').append(`<li class="list${p++}" >This is a list item thing.</li>`);
-        
+        $('ul').append(`<li>This is a list item thing.</li>`);
+
+        $(`ul li`).on('click', function () {
+            let randomR = Math.floor((Math.random() * 255) + 0);
+            let randomG = Math.floor((Math.random() * 255) + 0);
+            let randomB = Math.floor((Math.random() * 255) + 0);
+
+            $(this).css('background-color', `rgb(${randomR}, ${randomG}, ${randomB})`);
+
+        });
+
+        $(`ul li`).on('dblclick', function () {
+            this.remove();
+        });
+
+        console.log(`${p}`);
         event.preventDefault();
 
-        $('li').each(function ( p ) {
-            $(`.list${p}`).click(function () { 
-                let randomR = Math.floor((Math.random() * 255) + 0);
-                let randomG = Math.floor((Math.random() * 255) + 0);
-                let randomB = Math.floor((Math.random() * 255) + 0);
 
-                $(`.list${p}`).css('background-color', `rgb(${randomR}, ${randomG}, ${randomB})`);
-                event.preventDefault();
+        // $(`.list${p}`).each(function (p) {
+        //     $(`.list${p}`).click(function () { 
+        //         let randomR = Math.floor((Math.random() * 255) + 0);
+        //         let randomG = Math.floor((Math.random() * 255) + 0);
+        //         let randomB = Math.floor((Math.random() * 255) + 0);
 
-            });
+        //         $(`.list${p}`).css('background-color', `rgb(${randomR}, ${randomG}, ${randomB})`);
+        //     });
 
-            $(`.list${p}`).dblclick(function () { 
-                $(this).remove();
+        //     $(`.list${p}`).dblclick(function () { 
+        //         $(this).remove();
 
-                event.preventDefault();
-                
-            });
+        //     });
 
-            console.log(`${p}`);
+        //     console.log(`${p}`);
 
-        }); 
+        // }); 
 
         // Made for h2 element when hovering over.
         // $('h2').hover(
